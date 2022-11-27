e5df95de749cadb7923209cd8c488871077e1589
Aryan <aryan16072001236809@gmail.com>
2022-11-27 15:33:47 +0530

    Initialize Dad joke and Fetching Jokes from Api

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import JokeList from './JokeList';
+
+function App() {
+  return (
+    <div className="App">
+      <JokeList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/JokeList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+class JokeList extends Component {
+    static defaultProps={
+        numJokesToGet:10
+    };
+    constructor(props){
+        super(props);
+        this.state={jokes:[]};
+    }
+   async  componentDidMount(){
+        //Load Jokes
+        let jokes=[];
+        while(jokes.length<this.props.numJokesToGet){
+            let res= await axios.get("https://icanhazdadjoke.com/",{headers:{Accept:"application/json"}})
+        
+    jokes.push(res.data.joke);
+        }
+      this.setState({jokes:jokes});
+    }
+    
+  render() {
+    return (
+      <div className='JokeList'>
+        <h1>Dad Jokes</h1>
+        <div className='JokeList-jokes'>
+           {this.state.jokes.map(j=>(
+            <div>{j}</div>
+           ) )
+  }
+        </div>
+      </div>
+    )
+  }
+}
+export default JokeList;
