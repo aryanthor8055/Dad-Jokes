5760d334877b8d1d86e31a8684f755b16d835b17
Aryan <aryan16072001236809@gmail.com>
2022-11-28 23:34:24 +0530

    Final Styling and completed

--- src/JokeList.js
+++ src/JokeList.js
@@ -80,7 +80,7 @@ class JokeList extends Component {
         <div className='JokeList-sidebar'>
         <h1 className='JokeList-title'><span>Dad</span>Jokes</h1>
         <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'/>
-        <button className='JokeList-getmore' onClick={this.handleClick}>New Jokes</button>
+        <button className='JokeList-getmore' onClick={this.handleClick}>Fetch Jokes</button>
         </div>
         
         <div className='JokeList-jokes'>
